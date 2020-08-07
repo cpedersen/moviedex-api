f50840147f89fb2451b65fc3cde8faf206db224f
Catherine Pedersen <cathy.pedersen@gmail.com>
2020-08-06 22:11:29 -0400

    Implemented assignment

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Given a JSON file containing information about thousands of movies, create an Ex
 ### Requirements
 1. Users can search for Movies by genre, country or avg_vote
 - The endpoint is GET /movie
-- The search options for genre, country, and/or average vote are - provided in query string parameters.
+- The search options for genre, country, and/or average vote are  provided in query string parameters.
 - When searching by genre, users are searching for whether the Movie's genre includes a specified string. The search should be case insensitive.
 - When searching by country, users are searching for whether the Movie's country includes a specified string. The search should be case insensitive.
 - When searching by average vote, users are searching for Movies with an avg_vote that is greater than or equal to the supplied number.
@@ -17,3 +17,11 @@ Given a JSON file containing information about thousands of movies, create an Ex
 
 2. The endpoint only responds when given a valid Authorization header with a Bearer API token value.
 3. The endpoint should have general security in place such as best practice headers and support for CORS.
+
+### Example Commands
+* http://localhost:8000/movie
+* http://localhost:8000/movie?country=italy
+* http://localhost:8000/movie?genre=comedy
+* http://localhost:8000/movie?genre=comedy&country=italy
+* http://localhost:8000/movie?genre=comedy&country=italy&avg_vote=5.4
+* http://localhost:8000/movie?avg_vote=5.4
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 //console.log('hello')
-
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const MOVIES = require('./movies-data-small.json')
 
+//console.log(process.env.API_TOKEN)
 const app = express()
 
 app.use(morgan('dev'))
@@ -12,12 +13,47 @@ app.use(morgan('dev'))
   res.send('Hello, world!')
 })*/
 
-function handleGetMovie(req, res) {
-  //let response = MOVIES;
-  //res.json(response);
+app.use(function validateBearerToken(req, res, next) {
+  //const bearerToken = req.get('Authorization').split(' ')[0]
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+  if (!authToken || authToken.split(' ')[0] !== apiToken) {
+      return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  //move to the next middleware
+  next()
+})
+
+app.use(function validateBearerToken(req, res, next) {
+  console.log('validate bearer token middleware')
+  // move to the next middleware
+  next()
+})
+
+function handleGetMovies(req, res) {
+  let response = MOVIES;
+  //console.log("MOVIE: " + MOVIES);
+  if (req.query.genre) {
+    response = response.filter(movie =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    )
+  }
+  if (req.query.country) {
+    response = response.filter(movie =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    )
+  }
+  if (req.query.avg_vote) {
+    console.log ("Number: " + req.query.avg_vote);
+    response = response.filter(movie =>
+      //movie.avg_vote >= req.query.avg_vote
+      Number(movie.avg_vote) >= Number(req.query.avg_vote)
+    )
+  }
+  res.json(response);
 }
 
-app.get('/movie', handleGetMovie)
+app.get('/movie', handleGetMovies)
 
 const PORT = 8000
 
